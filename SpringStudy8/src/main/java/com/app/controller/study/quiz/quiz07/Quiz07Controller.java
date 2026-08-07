93d5f990d46714344481e1f4d3a92f118858ae82
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-08-07 12:25:48 +0900

    퀴즈9 요청경로 추가(퀴즈7), redirect, forward

--- SpringStudy8/src/main/java/com/app/controller/study/quiz/quiz07/Quiz07Controller.java
+++ SpringStudy8/src/main/java/com/app/controller/study/quiz/quiz07/Quiz07Controller.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,16 +14,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/quiz07")
 public class Quiz07Controller {
 
-	@GetMapping("/listTest")
-	public String listTest(@RequestParam String type, Model model) {
+	@GetMapping({ "/listTest", "/listTest/{pathVar}" })
+	public String listTest(@RequestParam(required = false) String type, @PathVariable(required = false) String pathVar,
+			Model model) {
 
+		String target = type != null ? type : pathVar != null ? pathVar : "base";
 		List<Object> carrigeList = new ArrayList<>();
 
-		if (type.equals("member")) {
+		if (target.equals("member")) {
 			for (int i = 1; i <= 5; i++) {
 				carrigeList.add(new MemberDTO("아이디" + i, "비번" + i, "이름" + i));
 			}
-		} else {
+		} else if (target.equals("str")) {
 			for (int i = 0; i < 10; i++) {
 				carrigeList.add("스트링 리스트입니다.");
 			}
