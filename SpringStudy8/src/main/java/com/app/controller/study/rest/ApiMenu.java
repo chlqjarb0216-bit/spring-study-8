6e1f4cf4f95025adedaa62c6f86fc0ed95f5aaf3
chlqjarb0216-bit <chlqjarb0216@gmail.com>
2026-08-19 14:30:24 +0900

    REST API 응답 response 객체 활용 (header, body 양식), 제네릭

---
+++ SpringStudy8/src/main/java/com/app/controller/study/rest/ApiMenu.java
@@ -0,0 +1,11 @@
+package com.app.controller.study.rest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ApiMenu {
+	String name;
+	int price;
+}
